27990de3e5c486fc16af8dc4e244f94bce7f48dd
Aminah <wereaminah8@gmail.com>
2019-09-02 16:48:01 -0700

    Javascript Complete

---
+++ date.js
@@ -0,0 +1,16 @@
+function datefunction(){
+console.log("Enter your year of Birth");
+process.stdin.once('data', (number)=>{
+
+var age=2019-number;
+if (age<18){ 
+    console.log("You're a MINOR!");
+ }else if(age<36){ 
+    console.log("You're a YOUTH");
+ } 
+ else{ 
+    console.log("You're an ELDER!"); 
+}
+ }
+ ) 
+}   console.log(datefunction());
